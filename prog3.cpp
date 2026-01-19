7d244468aa30ad25a3e3bcbeeb2e5140978bbedb
Arnav Sachdeva <arnavsachdeva@proton.me>
2026-01-19 10:35:52 +0530

    Third file

---
+++ prog3.cpp
@@ -0,0 +1,18 @@
+//Enter roll no.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rno,m1,m2,m3;
+    // printf("\n Enter rno: ");
+    cout<<"\n Enter rno;";
+
+    // scanf("%d",&rno);
+    cin>>rno>>m1>>m2>>m3;
+
+    //printf("\n your rollno is %d",rno);
+    cout<<"Your rno is:"<<rno<<endl<<m1<<endl<<m2<<endl<<m3;
+    return 0;
+}
