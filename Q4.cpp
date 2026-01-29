78f87018cd381c803c59648b0b1c3e2502cb4328
Arnav Sachdeva <arnavsachdeva@proton.me>
2026-01-29 13:03:10 +0530

    Q4

--- Q4.cpp
+++ Q4.cpp
@@ -1,3 +1,6 @@
+/*A shopkeeper wants to calculate the total bill amount after applying a 20% discount on the purchase.
+Implement a solution to accept item no., quantity, and unit price. Compute the amount and apply 20% discount.*/
+
 #include<iostream>
 #include<iomanip>
 using namespace std;
