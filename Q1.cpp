472f54bb8cb4bb0a3363c705fe106e822e50b3fd
Arnav Sachdeva <arnavsachdeva@proton.me>
2026-03-17 20:14:16 +0530

    Made the 1st question better

--- Q1.cpp
+++ Q1.cpp
@@ -1,23 +1,23 @@
-/* Q1. A teacher wants to calculate the average marks of three students to determine the class performance.
-Implement a solution to accept three numbers and compute their average. */
-
+// Q1: Calculate the average marks of three students.
 #include<iostream>
 #include<iomanip>
-
 using namespace std;
 
 int main()
-{
-    float x,y,z,average;
-    cout<<"\nEnter the marks of first student: ";
-    cin>>x;
-    cout<<"Enter the marks of second student: ";
-    cin>>y;
-    cout<<"Enter the marks of third student: ";
-    cin>>z;
 
-    average = (x+y+z)/3;
+{   
+    int i=1;
+    float marks,total=0.0f;
+    while(i<=3)
+    {
+    cout<<"Enter marks of the student"<<i<<": ";
+    cin>>marks;
+    total += marks;
+    i++;
+}
+float average;
+average = total/3.0f;
 
-    cout<<"The average marks of the three students are: "<<fixed<<setprecision(2)<<average;
-    return 0;
+cout<<"The average marks of the three students are: "<<setprecision(4)<<average<<endl;
+return 0;
 }
